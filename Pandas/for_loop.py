ffbafa3d6bf48927b599d013ae25c7b18cdd93fe
Ziqiao John Lin <linziqiao0620@gmail.com>
2018-02-20 11:33:12 -0800

    Add files via upload

---
+++ Pandas/for_loop.py
@@ -0,0 +1,7 @@
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+#for index, area in enumerate(areas, start = 1) :
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
